ad2587b29bae8ac7132ba629a9f1a7868ef84be9
Le Quoc Thai <20492546+tnfsmith@users.noreply.github.com>
2024-01-20 18:16:18 +0000

    ok

--- serp.py
+++ serp.py
@@ -1,68 +1,46 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-from selenium_stealth import stealth
 import math
 
-
-def search_google_web_automation(query, num_results):
+def search_google_requests(query, num_results):
     # Enforce limits on num_results
-    if num_results < 5:
-        num_results = 5
-    elif num_results > 100:
-        num_results = 100
+    num_results = min(max(num_results, 5), 100)
 
     # Calculate the number of pages
     n_pages = math.ceil(num_results / 10)
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option("useAutomationExtension", False)
-    driver = webdriver.Chrome(options=chrome_options)
 
-    stealth(
-        driver,
-        languages=["en-US", "en"],
-        vendor="Google Inc.",
-        platform="Win32",
-        webgl_vendor="Intel Inc.",
-        renderer="Intel Iris OpenGL Engine",
-        fix_hairline=True,
-    )
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
     results = []
     counter = 0
     for page in range(1, n_pages + 1):
-        url = (
-            "http://www.google.com/search?q="
-            + str(query)
-            + "&start="
-            + str((page - 1) * 10)
-        )
+        url = f"http://www.google.com/search?q={query}&start={(page - 1) * 10}"
 
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, "html.parser")
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
         search = soup.find_all("div", class_="yuRUbf")
 
         for h in search:
             if counter == num_results:
                 break
             counter += 1
-            title = h.a.h3.text
-            link = h.a.get("href")
-            rank = counter
-            results.append(
-                {
-                    "title": title,
-                    "url": link,
-                    "domain": urlparse(link).netloc,
-                    "rank": rank,
-                }
-            )
+            title_element = h.find("h3")
+            if title_element:
+                title = title_element.text
+                link = h.a.get("href")
+                rank = counter
+                results.append(
+                    {
+                        "title": title,
+                        "url": link,
+                        "domain": urlparse(link).netloc,
+                        "rank": rank,
+                    }
+                )
 
-        if counter == num_results:
-            break
+            if counter == num_results:
+                break
 
-    driver.quit()
     return results
